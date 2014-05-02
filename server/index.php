5a8e2df5a0cf6a47bd7a3a962ee3774f57cbbda1
Miguel Julián <mjulian@zentyal.com>
2014-05-02 20:12:23 +0200

    Added autoload class function

--- server/index.php
+++ server/index.php
@@ -1,5 +1,16 @@
 <?php
     require 'RestServer.php';
+
+    // Registering a class to load server controllers
+    spl_autoload_register('apiAutoload');
+    function apiAutoload($classname)
+    {
+        if (preg_match('/[a-zA-Z]+Controller$/', $classname)) {
+            include __DIR__ . '/controllers/' . $classname . '.php';
+            return true;
+    }
+
+    // Handle the request
     $server = new RestServer();
     $server->handleRequest();
 ?>
