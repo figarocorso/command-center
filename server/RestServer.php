8d5fb72bac74f171a465c9343aeb833b6634f1ac
Miguel Julián <mjulian@zentyal.com>
2014-05-03 18:13:38 +0200

    RestServer invokes the correct module method given by the request

--- server/RestServer.php
+++ server/RestServer.php
@@ -3,9 +3,39 @@
  * Handles HTTP request invoking the proper PHP function (we must load
  * those functional classes before sending HTTP request)
  */
+
+require_once 'Request.php';
+
 class RestServer {
     public function handleRequest() {
+        $request = new Request();
+        $this->exitIfBadRequest($request);
+        $result = $this->invokeControllerMethod($request);
+        var_dump($result);
+
         return("ok");
+    }
+
+    private function exitIfBadRequest($request) {
+        if (! $request->success()) {
+            //FIXME: Return error data
+            return False;
+            exit;
+        }
+    }
+
+    private function invokeControllerMethod($request) {
+        $controllerName = $request->module() . 'Controller';
+        if (class_exists($controllerName)) {
+            $controller = new $controllerName();
+            $actionName = $request->action() . $request->method();
+
+            if (method_exists($controller, $actionName)) {
+                return $controller->$actionName($request);
+            }
+        }
+
+        return False;
     }
 }
 ?>
