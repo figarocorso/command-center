218ca87e16dd29b98ec0813d5aec117dbfb695d7
Miguel Julián <mjulian@zentyal.com>
2014-05-05 23:00:56 +0200

    Fixed database connection

--- server/database/DbConfiguration.php
+++ server/database/DbConfiguration.php
@@ -1,20 +1,14 @@
 <?php
-
 /**
  * This database classes need a database with these tables:
  * CREATE TABLE command_values (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, command_key VARCHAR(255), command_value VARCHAR(255));
  */
 class DbConfiguration {
-    protected $serverName;
-    protected $userName;
-    protected $passCode;
-    protected $dbName;
-
-    function Dbconfig() {
-        $this -> serverName = 'localhost';
-        $this -> userName = 'root';
-        $this -> passCode = 'foobar';
-        $this -> dbName = 'commandcenter';
+    function __construct() {
+        $this->servername = 'localhost';
+        $this->username = 'root';
+        $this->password = 'foobar';
+        $this->database = 'commandcenter';
     }
 }
 ?>
--- server/database/Mysql.php
+++ server/database/Mysql.php
@@ -4,43 +4,22 @@ include 'DbConfiguration.php';
 
 class Mysql extends DbConfiguration {
 
-    public $connectionString;
-    public $dataSet;
-    private $sqlQuery;
+    private $mysqli;
 
-    protected $databaseName;
-    protected $hostName;
-    protected $userName;
-    protected $passCode;
-
-    function __construct() {
-        $this->connectionString = NULL;
-        $this->sqlQuery = NULL;
-        $this->dataSet = NULL;
-
-        $databaseParameters = new DbConfiguration();
-        $this->databaseName = $databaseParameters->dbName;
-        $this->hostName = $databaseParameters->serverName;
-        $this->userName = $databaseParameters->userName;
-        $this->passCode = $databaseParameters->passCode;
-        $databaseParameters = NULL;
+    public function __construct() {
+        parent::__construct();
+        $this->mysqli = new mysqli( $this->servername, $this->username, $this->password, $this->database);
     }
 
-    function dbConnect()    {
-        $this->connectionString = mysql_connect($this->serverName, $this->userName, $this->passCode);
-        mysql_select_db($this->databaseName, $this->connectionString);
-
-        return $this->connectionString;
+    public function dbDisconnect() {
+        $this->mysqli = NULL;
     }
 
-    function dbDisconnect() {
-        $this->connectionString = NULL;
-        $this->sqlQuery = NULL;
-        $this->dataSet = NULL;
-        $this->databaseName = NULL;
-        $this->hostName = NULL;
-        $this->userName = NULL;
-        $this->passCode = NULL;
+    public function getValue($key) {
+        $row = $this->mysqli->query("SELECT command_value FROM command_values WHERE command_key='$key'");
+        $result = $row->fetch_array();
+
+        return $result["command_value"];
     }
 }
 ?>
