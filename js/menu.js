db10a5c4f97eff25dbaf45f38b753dfdb4b6d0cb
Miguel Julián <mjulian@zentyal.com>
2014-05-08 23:38:50 +0200

    First upper menu draft
     * Hide/Show arrow is working

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+function showMenu() {
+    changeArrow();
+    $(".menu").toggle(100);
+}
+
+function changeArrow() {
+    downImageShowed = ($("#showMenuArrow").attr('src').indexOf("down") > -1);
+
+    if (downImageShowed)
+        $("#showMenuArrow").attr('src', "img/up-arrow.png");
+    else
+        $("#showMenuArrow").attr('src', "img/down-arrow.png");
+}
