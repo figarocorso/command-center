d3bf4f2028ec8187af9bae714aa4e3d43b296d45
Miguel Julián <mjulian@zentyal.com>
2014-05-16 08:51:52 +0200

    Subtasks backend controller

--- server/controllers/TasksController.php
+++ server/controllers/TasksController.php
@@ -4,6 +4,8 @@ require_once("database/Mysql.php");
 
 class TasksController
 {
+    const SUBTASKS_GLUE = '[separator]';
+
     public function getTasktitle($request) {
         $mysql = new Mysql();
 
@@ -21,6 +23,49 @@ class TasksController
         $mysql->setValue("tasktitle", $parameters['tasktitle']);
 
         return $mysql->getValue("tasktitle");
+    }
+
+    public function getSubtasks($request) {
+        $mysql = new Mysql();
+        $subtasks = $mysql->getValue("subtasks");
+
+        return $this->parseSubtasks($subtasks);
+    }
+
+    public function postSubtasks($request) {
+        $parameters = $request->parameters();
+
+        if (!isset($parameters['subtasks'])) {
+            return False;
+        }
+
+        $subtasksString = $this->convertSubtasksToString($parameters['subtasks']);
+
+        $mysql = new Mysql();
+        $mysql->setValue("subtasks", $subtasksString);
+
+        return $mysql->getValue("subtasks");
+    }
+
+
+    /* Private helper functions */
+
+    private function parseSubtasks($subtasksString) {
+        $subtasks = explode(self::SUBTASKS_GLUE, $subtasksString);
+
+        $subtasksArray = array();
+        foreach ($subtasks as $subtask) {
+            $done = substr($subtask, 0, 3);
+            $done = (strpos($done, 'd') === FALSE) ? "undone" : "done";
+            $name = ucfirst(substr($subtask, 3));
+            $subtasksArray[] = array("done" => $done, "name" => $name);
+        }
+
+        return $subtasksArray;
+    }
+
+    private function convertSubtasksToString($subtasksArray) {
+        return implode(self::SUBTASKS_GLUE, $subtasksArray);
     }
 }
 ?>
